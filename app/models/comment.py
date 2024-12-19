43def8d3ef9eee317244f684f9cf3f2304ffa5eb
Victor Wilson <63019070+dsvwilson@users.noreply.github.com>
2024-12-19 11:09:55 +0100

    Created database models for the CommentData table for comments

---
+++ app/models/comment.py
@@ -0,0 +1,8 @@
+from sqlmodel import SQLModel, Field
+from datetime import date
+
+class CommentData(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    date: date
+    content: str
+    post_id: int = Field(foreign_key="blogpostdata.id")

9e05a34c1389df13cd70663b5aae4e33b0a38a27
Victor Wilson <63019070+dsvwilson@users.noreply.github.com>
2024-12-23 03:59:08 +0100

    Added Authentication for critical endpoints (read a single comment and delete a comment)

--- app/api/comment.py
+++ app/api/comment.py
@@ -1,14 +1,25 @@
-from fastapi import APIRouter, HTTPException
+# external module import
+from typing import Annotated
+from fastapi import APIRouter, Depends, HTTPException
+
+# internal module imports
+from app.services.classes.user import User
+from app.models.comment import CommentData
 from app.services.classes.comment import Comment
 from app.services.schema.comment import CommentSchema
-from app.models.comment import CommentData
 from app.database.post_db import read_post
 from app.database.comment_db import new_comment, read_comments, read_comment, delete_comment_db
 
 router = APIRouter()
 
+# get the current user from the class methods for authentication purposes
+async def get_current_active_user(current_user: Annotated[User, Depends(User.get_current_user)]):
+        if current_user.disabled:
+            raise HTTPException(status_code=400, detail="Inactive user")
+        return current_user
+
 # create a new comment
-@router.post("/posts/{post_id}/comment/new", tags=["Comments"])
+@router.post("/posts/{post_id}/comment/new")
 async def post_comment(create: CommentSchema, post_id: int) -> str:
     # return an error if the post does not exist
     Comment.post_check(post_id, read_post, post_id, exception=HTTPException)
@@ -19,7 +30,7 @@ async def post_comment(create: CommentSchema, post_id: int) -> str:
     return f"Comment {comment_id} successfully added for post {post_id}"
 
 # get all the comments associated with a certain post
-@router.get("/posts/{post_id}/comments", tags=["Comments"])
+@router.get("/posts/{post_id}/comments")
 async def get_comments(post_id: int, offset: int | None = 0, limit: int | None = 10) -> list[CommentData]:
     # return an error if the post does not exist
     Comment.post_check(post_id, read_post, post_id, exception=HTTPException)
@@ -27,8 +38,10 @@ async def get_comments(post_id: int, offset: int | None = 0, limit: int | None =
     return read_comments(post_id, offset, limit)
 
 # get a comment by its ID - if unavailable, return a 404 error message
-@router.get("/posts/comment/{comment_id}", tags=["Comments"])
-async def get_comment(comment_id: int) -> CommentData:
+#
+# requires authentication
+@router.get("/posts/comment/{comment_id}")
+async def get_a_comment(current_user: Annotated[User, Depends(get_current_active_user)], comment_id: int) -> CommentData:
     comment = read_comment(comment_id)
     if comment:
         return comment
@@ -36,8 +49,10 @@ async def get_comment(comment_id: int) -> CommentData:
         raise HTTPException(status_code=404, detail=f"Comment {comment_id} does not exist")
 
 # delete a comment
-@router.delete("/posts/comment/{comment_id}", tags=["Comments"])
-def delete_comment(comment_id: int):
+#
+# requires authentication
+@router.delete("/posts/comment/{comment_id}")
+def delete_comment(current_user: Annotated[User, Depends(get_current_active_user)], comment_id: int):
     delete_operation = delete_comment_db(comment_id)
     if delete_operation:
         return delete_operation

6c94edcb439d9549a595632abc173132cb6498c8
Victor Wilson <63019070+dsvwilson@users.noreply.github.com>
2024-12-23 03:57:34 +0100

    Added the router for User & Authentication

--- app/main.py
+++ app/main.py
@@ -1,16 +1,10 @@
 from fastapi import FastAPI
-from app.api import blogpost, comment
+from app.api import blogpost, comment, user
 
-api_groups = [
-    {
-        "name": "Posts"
-    },
-    {
-        "name": "Comments"
-    }
-]
+api_groups = [{"name": "Auth"}, {"name": "Posts"}, {"name": "Comments"}]
 
 app = FastAPI(openapi_tags=api_groups)
 
+app.include_router(user.router, tags=["Auth"], prefix="/v1")
 app.include_router(blogpost.router, tags=["Posts"], prefix="/v1")
 app.include_router(comment.router, tags=["Comments"], prefix="/v1")

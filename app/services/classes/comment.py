4a2277431cff9b202ccb0530308ae267b92f6d9a
Victor Wilson <63019070+dsvwilson@users.noreply.github.com>
2024-12-23 04:02:57 +0100

    Slight adjustment to the overall structure of the file and lines of code

--- app/services/classes/comment.py
+++ app/services/classes/comment.py
@@ -1,5 +1,6 @@
 import datetime
 
+
 class Comment:
     def __init__(self, date="", content=""):
         self.date = date
@@ -9,7 +10,7 @@ class Comment:
     @property
     def date(self):
         return self._date
-    
+
     @date.setter
     def date(self, date):
         self._date = date
@@ -17,13 +18,13 @@ class Comment:
     @property
     def content(self):
         return self._content
-    
+
     @content.setter
     def content(self, content):
         self._content = content.strip()
 
     # static methods
-    @staticmethod # returns a 404 error for when a post is not found
+    @staticmethod  # returns a 404 error for when a post is not found
     def post_check(post_id: int, read_post, *args, exception):
         if not read_post(*args):
             raise exception(status_code=404, detail=f"Post {post_id} does not exist")

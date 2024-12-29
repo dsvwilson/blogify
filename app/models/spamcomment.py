e181a8daaebd87a04de7a180b464f05998941f6a
Victor Wilson <63019070+dsvwilson@users.noreply.github.com>
2024-12-29 03:07:21 +0100

    Defined a table model for spamcomments

---
+++ app/models/spamcomment.py
@@ -0,0 +1,9 @@
+from sqlmodel import SQLModel, Field
+from datetime import date
+
+class SpamCommentData(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    date: date
+    content: str
+    post_id: int = Field(foreign_key="blogpostdata.id")
+    comment_genuinity: int
